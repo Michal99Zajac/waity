ce31a4f5c1cbaba70493a8d2a091d8c0c2520fec
Michal99Zajac <michal.99.zajac@gmail.com>
2021-04-15 21:53:33 +0200

    add button component to the project

--- src/frontend/src/components/fundamental/actions/Button.stories.tsx
+++ src/frontend/src/components/fundamental/actions/Button.stories.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+import { Button } from './Button'
 
 
 const stories = storiesOf('actions', module)
+stories.addDecorator(withKnobs)
 stories.add('Button', () => {
-    return (
-        <div>Test</div>
-    )
+  const typeOptions = {
+    button: 'button',
+    submit: 'submit',
+    none: undefined
+  }
+
+  const colorOptions = {
+    black: 'black',
+    blue: 'blue',
+    yellow: 'yellow',
+    none: undefined
+  }
+
+  const desc = text('desc', 'click me!')
+  const type: any = select('type', typeOptions, 'none')
+  const color: any = select('color', colorOptions, 'none')
+  const onClick = action('onClick Button')
+  const small = boolean('small', false)
+  const disabled = boolean('disabled', false)
+
+  return (
+    <Button disabled={disabled} color={color} small={small} type={type} onClick={onClick} desc={desc} />
+  )
 })
