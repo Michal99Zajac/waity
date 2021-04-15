f5f0c327fc9e113395f5272d73612afe59d450ce
Michal99Zajac <michal.99.zajac@gmail.com>
2021-04-15 20:00:20 +0200

    add storybook to the project

---
+++ src/frontend/src/components/fundamental/actions/Button.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+
+const stories = storiesOf('actions', module)
+stories.add('Button', () => {
+    return (
+        <div>Test</div>
+    )
+})
