e120949d6754f00a2294ffd6a7b098a3841e540b
Michal99Zajac <michal.99.zajac@gmail.com>
2021-04-16 11:10:23 +0200

    add Warning Component to the project

--- src/frontend/src/components/fundamental/feedback/warning/warning.stories.tsx
+++ src/frontend/src/components/fundamental/feedback/warning/warning.stories.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs'
+import { array, text, withKnobs } from '@storybook/addon-knobs'
 import { Warning } from './warning'
 
 
 const stories = storiesOf('feedback', module)
 stories.addDecorator(withKnobs)
 stories.add('warning', () => {
+  const msg = array('msg', ['Error: Bad Password'])
+
   return (
-    <Warning msg='hello' />
+    <div style={{
+      position: 'relative',
+      top: '200px',
+      width: '100vw',
+      height: '100vh'
+    }}>
+      <Warning msg={msg} />
+    </div>
   )
 })
--- src/frontend/src/components/fundamental/feedback/warning/warning.tsx
+++ src/frontend/src/components/fundamental/feedback/warning/warning.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import styles from './warning.module.sass'
 import { ReactComponent as Svg } from '../../../../assets/svg/basic/important.svg'
 
 
 type WarningTypes = {
-  msg: string
+  msg: string | string[]
 }
 
+/**
+ * Warning Component - svg with hide text for for information about error
+ * 
+ * @param {object} props Object with props like
+ * - msg (string | string[]) - msg or msgs for display
+ * @returns JSX Warning Component
+ */
 export function Warning(props: WarningTypes) {
   return (
-    <div>
+    <div className={styles.warning}>
       <Svg />
+      <p>
+        { Array.isArray(props.msg) && props.msg.length != 1 ? props.msg.map((message) => {
+          return (
+            <>
+              • {message}<br/>
+            </>
+          )
+        }) : props.msg }
+      </p>
     </div>
   )
 }
