f12ff767d65554a820b9f6aca2d10035dad5bd69
Michal99Zajac <michal.99.zajac@gmail.com>
2021-04-21 19:41:54 +0200

    frontend: add measure options to SectionLabel Component

--- src/frontend/src/components/fundamental/titles-n-text/section-label/section-label.stories.tsx
+++ src/frontend/src/components/fundamental/titles-n-text/section-label/section-label.stories.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { boolean, text, withKnobs } from '@storybook/addon-knobs'
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
 import { SectionLabel } from './section-label'
 
 
 const stories = storiesOf('titles-n-text', module)
 stories.addDecorator(withKnobs)
 stories.add('section-label', () => {
+  const sizeOptions = {
+    s: 's',
+    m: 'm',
+    l: 'l',
+    xl: 'xl'
+  }
+
   const label = text('label', 'very long label')
   const firm = boolean('firm', false)
+  const size: any = select('size', sizeOptions, sizeOptions['m'])
 
   return (
     <div style={{
@@ -16,7 +24,7 @@ stories.add('section-label', () => {
       height: '100vh',
       backgroundColor: '#ebebeb'
     }}>
-      <SectionLabel firm={firm} label={label} />
+      <SectionLabel size={size} firm={firm} label={label} />
     </div>
   )
 })
--- src/frontend/src/components/fundamental/titles-n-text/section-label/section-label.tsx
+++ src/frontend/src/components/fundamental/titles-n-text/section-label/section-label.tsx
@@ -4,7 +4,8 @@ import styles from './section-label.module.sass'
 
 type SectionLabelTypes = {
   label: string,
-  firm?: boolean
+  firm?: boolean,
+  size?: 's' | 'm' | 'l' | 'xl'
 }
 
 /**
@@ -13,11 +14,15 @@ type SectionLabelTypes = {
  * @param {object} props Object with props like
  * - label (string) - text of label
  * - firm (bool) - indicates if label is for firm side
+ * - size (string) - indicates the size of the label
  * @returns JSX SectionLabel Component
  */
 export function SectionLabel(props: SectionLabelTypes): JSX.Element {
   return (
-    <p className={`${styles.selectionLabel} ${props.firm ? styles.firm : ''}`}>
+    <p className={`${styles.selectionLabel}
+                  ${props.firm ? styles.firm : ''}
+                  ${props.size ? styles[props.size] : styles.m}
+    `}>
       {props.label}
     </p>
   )
