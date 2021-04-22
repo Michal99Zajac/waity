f84217c63c7c923fb9b8991ef7facc7491a1cd25
Michal99Zajac <michal.99.zajac@gmail.com>
2021-04-22 15:49:56 +0200

    frontend: add complete OfferCard component to the project

--- src/frontend/src/components/fundamental/structure/offer-card/offer-card.stories.tsx
+++ src/frontend/src/components/fundamental/structure/offer-card/offer-card.stories.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs'
+import { withKnobs, text, select, number } from '@storybook/addon-knobs'
 import { OfferCard } from './offer-card'
+import RestaurantOne from '../../../../assets/img/restaurants/restaurant_one.jpg'
+import RestaurantTwo from '../../../../assets/img/restaurants/restaurant_two.jpg'
 
 
 const stories = storiesOf('structure', module)
 stories.addDecorator(withKnobs)
 stories.add('offer-card', () => {
+  const srcOptions = {
+    imageOne: RestaurantOne,
+    imageTwo: RestaurantTwo
+  }
+
+  const to = text('to', '#')
+  const alt = text('alt', 'image')
+  const label = text('label', 'restaurant')
+  const src = select('src', srcOptions, srcOptions.imageOne)
+  const caption = text('caption', 'caption')
+  const location = text('location', 'location')
+  const price = text('price', 'price')
+  const rating = number('rating', 3)
+
   return (
-    <OfferCard />
+    <Router>
+      <OfferCard rating={rating} caption={caption} location={location} price={price} src={src} to={to} alt={alt} label={label} />
+    </Router>
   )
 })
--- src/frontend/src/components/fundamental/structure/offer-card/offer-card.tsx
+++ src/frontend/src/components/fundamental/structure/offer-card/offer-card.tsx
@@ -1,10 +1,58 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './offer-card.module.sass'
+import { SectionLabel } from '../../titles-n-text/section-label/section-label'
+import { Caption } from '../../titles-n-text/caption/caption'
+import { Paragraph } from '../../titles-n-text/paragraph/paragraph'
+import { StarRating } from '../../feedback/star-rating/star-rating'
+import { ReactComponent as CashSvg } from '../../../../assets/svg/basic/cash.svg'
+import { ReactComponent as PlaceholderSvg } from '../../../../assets/svg/basic/placeholder.svg'
 
 
-type OfferCardTypes = {}
+type OfferCardTypes = {
+  to: string,
+  src: string,
+  alt: string,
+  label: string,
+  caption: string,
+  location: string,
+  price: string,
+  rating: number
+}
 
+/**
+ * OfferCard Component - card with link for display brief of offert
+ * 
+ * @param {object} props Object with props like
+ * - to (string) - path to location
+ * - alt (string) - alternate text for an image, if the image cannot be displayed
+ * - src (string) - path to the image
+ * - label (string) - main label of card
+ * - caption (string) - submain label of card
+ * - location (string) - text with placeolder svg
+ * - price (string) - text with cash svg
+ * - rating (number) - rating number
+ * @returns JSX OfferCard Component
+ */
 export function OfferCard(props: OfferCardTypes): JSX.Element {
   return (
-    <div>Hello From OfferCard</div>
+    <Link className={styles.offerCard} to={props.to}>
+      <div className={styles.offerContainer}>
+        <StarRating scale={5} rating={props.rating} label='reviews' />
+        <img src={props.src} alt={props.alt}></img>
+        <div className={styles.offerInfo}>
+          <SectionLabel offer label={props.label} />
+          <Caption content={props.caption} />
+          <span>
+            <PlaceholderSvg />
+            <Paragraph size='s'>{props.location}</Paragraph>
+          </span>
+          <span>
+            <CashSvg />
+            <Paragraph size='s'>{props.price}</Paragraph>
+          </span>
+        </div>
+      </div>
+    </Link>
   )
 }
