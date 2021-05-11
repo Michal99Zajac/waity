aceb99c21c9878c8c071a63a57d3260116681340
Michal99Zajac <michal.99.zajac@gmail.com>
2021-05-11 13:29:50 +0200

    backend: add password hashing to the auth

---
+++ src/backend/src/bcrypt.config.ts
@@ -0,0 +1,3 @@
+const saltRounds = Number(process.env.SALT_ROUNDS) || 10
+
+export default saltRounds
--- src/backend/src/entities/user.entity.ts
+++ src/backend/src/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm'
 import { Expose } from 'class-transformer'
-import { MinLength, IsEmail } from 'class-validator'
+import { MinLength, IsEmail, MaxLength, Matches } from 'class-validator'
 
 
 @Entity()
@@ -12,10 +12,14 @@ export class User {
 
   @Expose()
   @IsEmail()
+  @MaxLength(255, { message: 'email is to long'})
   @Column({ name: 'email'})
   email!: string
 
   @MinLength(7, { message: 'to short password' })
+  @MaxLength(100, { message: 'password is to long' })
+  @Matches(/.*[1-9].*$/, {message: 'password must contain number'})
+  @Matches(/.*[A-Z].*$/, { message: 'password must contain capital letter'})
   @Column()
   password!: string
 }
