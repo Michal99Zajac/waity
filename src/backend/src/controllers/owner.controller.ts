b559f1339df1d8f2e095d89297170cb72e8b41ac
Michal99Zajac <michal.99.zajac@gmail.com>
2021-05-26 15:03:15 +0200

    backend: add getOwnerAddress to the OwnerController

--- src/backend/src/controllers/owner.controller.ts
+++ src/backend/src/controllers/owner.controller.ts
@@ -6,6 +6,9 @@ import { Restaurant } from '../entities/restaurant.entity'
 
 
 class OwnerController {
+  /**
+   * get owner basic information like name and lastname by logged restaurantID
+   */
   async getOwnerBasic(req: Request, res: Response, next: NextFunction) {
     const connection = conn()
 
@@ -20,6 +23,26 @@ class OwnerController {
       }) : next(new BadRequest())
 
     res.status(200).json(owner)
+  }
+
+  /**
+   * get owner basic information with they address
+   */
+  async getOnwerAddress(req: Request, res: Response, next: NextFunction) {
+    const connection = conn()
+
+    // get owner with address by logged restaurant id
+    const ownerWithAddress = req.user instanceof Restaurant ?
+      await connection.getRepository(Owner).findOne({
+        relations: ['address'],
+        where: {
+          restaurant: {
+            id: req.user.id
+          }
+        }
+      }) : next(new BadRequest())
+
+    res.status(200).json(ownerWithAddress)
   }
 }
 
--- src/backend/src/routers/owner.router.ts
+++ src/backend/src/routers/owner.router.ts
@@ -8,4 +8,6 @@ const router = Router()
 
 router.get('/owners', passport.authenticate('restaurant-jwt'), guard(['restaurant']), ownerController.getOwnerBasic)
 
+router.get('/owners/address', passport.authenticate('restaurant-jwt'), guard(['restaurant']), ownerController.getOnwerAddress)
+
 export default router
