65d2df9f4c64961f5f7d265b0d91547c0d662a6e
Michal99Zajac <michal.99.zajac@gmail.com>
2021-06-07 19:55:57 +0200

    frontend: add media query to 404 page

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-d
 import { initState, reducer } from './reducer'
 import AuthContext from './context/auth-context'
 import LoggedRoute from './hoc/logged-route'
+import RegisterRoute from './hoc/register-route'
 
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import NotFound from './pages/404/not-found'
+import UserRegister from './pages/UserRegister/user-register'
 
 import './App.sass'
 
@@ -25,6 +27,7 @@ function App() {
           <Switch>
             <LoggedRoute path='/login' component={Login} />
             <LoggedRoute path='/signup' component={Signup} />
+            <RegisterRoute path='/user-register' component={UserRegister} />
             <Route component={NotFound} />
           </Switch>
         </Router>
---
+++ src/frontend/src/pages/UserRegister/user-register.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function UserRegister() {
+  return (
+    <div>hello</div>
+  )
+}
