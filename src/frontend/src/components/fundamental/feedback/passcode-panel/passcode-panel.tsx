93d4a0e7d2028d7709afda7bb1391d053fb38b65
Michal99Zajac <michal.99.zajac@gmail.com>
2021-06-13 10:57:37 +0200

    frontend: add animation to panel

--- src/frontend/src/components/fundamental/feedback/passcode-panel/passcode-panel.tsx
+++ src/frontend/src/components/fundamental/feedback/passcode-panel/passcode-panel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Paragraph } from '../../titles-n-text/paragraph/paragraph'
 import { Heading } from '../../titles-n-text/heading/heading'
 import { Button } from '../../actions/button/button'
@@ -19,32 +19,48 @@ type PasscodePanelTypes = {
  */
 export function PasscodePanel(props: PasscodePanelTypes): JSX.Element {
   const ref = useRef<HTMLDivElement>(null)
+  const blur = useRef<HTMLDivElement>(null)
   const regex: RegExp = /^[1-9]{7}$/
 
-  function closePanel() { ref.current?.remove() }
+  function closePanel() {
+    ref.current?.remove()
+    blur.current?.remove()
+  }
 
   useEffect(() => {
-    ref.current?.blur()
+    const numbers = document.querySelectorAll(`.${styles.number}`)
+
+    console.log(numbers)
+
+    for (const idx in props.passcode.split('')) {
+      setTimeout(() => {
+        numbers[idx].setAttribute('class', `${styles.number} ${styles.flip}`)
+      }, (+idx + 1) * 600)
+    }
   }, [])
 
   if (!regex.test(props.passcode)) { return <div>Error: passcode is incorrect</div> }
 
   return (
-    <div ref={ref} className={styles.passcodePanel}>
-      <div className={styles.panel}>
-        <Heading className={styles.heading} size='l' color='white' label='Thank You to Join Us!' />
-        <div className={styles.passcode}>
-          { props.passcode.split('').map(num => (
-            <div className={styles.number}>{num}</div>
-          ))}
+    <>
+      <div ref={blur} className={styles.blur}></div>
+      <div ref={ref} className={styles.passcodePanel}>
+        <div className={styles.panel}>
+          <Heading className={styles.heading} size='l' color='white' label='Thank You for Joining Us!' />
+          <div className={styles.passcode}>
+            { props.passcode.split('').map(num => (
+              <div className={styles.number}>{num}</div>
+            ))}
+          </div>
+          <Paragraph size='l' color='black' className={styles.info}>
+            This is your passcode.
+            Remember this because Your Restaurant will need it to login your firm to the restaurant application.
+            Please stay and complete registration.
+            Thank You Again and welcome to the waity family!
+          </Paragraph>
+          <Button className={styles.button} desc='got it!' onClick={closePanel} color='white' />
         </div>
-        <Paragraph size='l' color='black' className={styles.info}>
-          This is your passcode.
-          Remember this because Your Restaurant will need it to login your firm to the restaurant application.
-          Thank You Again and welcome to the waity family!
-        </Paragraph>
-        <Button className={styles.button} desc='got it!' onClick={closePanel} color='white' />
       </div>
-    </div>
+    </>
   )
 }
